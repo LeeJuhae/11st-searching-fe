ef3c51ca98add37fa053bf72704b90dcb97e098a
LeeJuhae <tlatmsdlwngo@naver.com>
2021-08-23 19:02:48 +0900

    feat: #20 사진 선택 기능 추가

--- src/views/Photo.ts
+++ src/views/Photo.ts
@@ -4,16 +4,31 @@ import { getSequenceArray } from '../utils/getSequenceArray';
 import '../assets/styles/Photo.css';
 
 export default class Photo extends Component {
-  setup() {}
+  setup() {
+    this.$state = { selectedIdx: 0 };
+  }
   template() {
+    const { selectedIdx } = this.$state;
     return `
     <div id='photo-wrapper'>
       <div id='photo-list'>
-      ${getSequenceArray(0, 10, 1)
-        .map((ele, idx) => `<img data-index=${idx} src=${photos['img' + (idx + 1)]}/>`)
-        .join('')}
+        ${getSequenceArray(0, 10, 1)
+          .map((ele, idx) => {
+            const className = selectedIdx == idx ? 'selected-photo' : 'photo';
+            return `<img class=${className} data-index=${idx} src=${photos[idx]}/>`;
+          })
+          .join('')}
+
+      </div>
+      <div id='selected-photo-wrapper'>
+        <img src=${photos[selectedIdx]} width="100%' height="100%"/>
       </div>
     </div>
 	  `;
+  }
+  setEvent() {
+    this.addEvent('click', '.photo', ({ target }) => {
+      this.setState({ selectedIdx: target.dataset.index });
+    });
   }
 }
